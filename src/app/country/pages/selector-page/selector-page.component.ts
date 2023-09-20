a5a7e5bde657125d7ea2115682841558f67e5ab9
Germán <german.palencia@adentia.es>
2023-09-20 17:58:55 +0200

    Conect service to recieve coutries data

--- src/app/country/pages/selector-page/selector-page.component.ts
+++ src/app/country/pages/selector-page/selector-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
-import { Region } from '../../interfaces/Country.interface';
+import { Region } from '../../interfaces/country.interface';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
@@ -35,8 +36,11 @@ export class SelectorPageComponent implements OnInit{
 
   onRegionChanged() {
     this.myForm.get('region')!.valueChanges
-    .subscribe(region => {
-      console.log({region})
+    .pipe(
+      switchMap(region => this.countriesService.getCoutriesByRegion(region))
+    )
+    .subscribe(countries => {
+      console.log({countries})
     });
   }
 
--- src/app/country/services/countries.service.ts
+++ src/app/country/services/countries.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 import { Region, SmallCoutry } from '../interfaces/country.interface';
+import { Observable, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,17 +10,28 @@ import { Region, SmallCoutry } from '../interfaces/country.interface';
 
 export class CountriesService {
 
+  private baseUrl: string = 'https://restcountries.com/v3.1';
+
   private _regions: Region[] = [Region.Africa, Region.America, Region.Asia, Region.Europa, Region.Oceania];
 
-  constructor() {}
+  constructor(
+    private http: HttpClient,
+  ) {}
 
   get regions(): Region[] {
     return [...this._regions];
   }
 
-  getCoutriesByRegion(region: Region): SmallCoutry[] {
+  getCoutriesByRegion(region: Region): Observable<SmallCoutry[]> {
 
-    return [];
+    if(!region) return of([]);
+
+    const url: string = `${this.baseUrl}/region/${region}?fields=cca3,name,borders`;
+
+    return this.http.get<SmallCoutry[]>(url)
+    .pipe(
+      tap(response => console.log({response}))
+    );
 
   }
 
