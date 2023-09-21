81470404394a8c95f2dfd1aa0e9e7f08251c3adf
Germán <german.palencia@adentia.es>
2023-09-21 12:59:01 +0200

    third selector

--- src/app/country/pages/selector-page/selector-page.component.ts
+++ src/app/country/pages/selector-page/selector-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region, SmallCoutry } from '../../interfaces/country.interface';
-import { switchMap, tap } from 'rxjs';
+import { filter, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
@@ -14,11 +14,12 @@ import { switchMap, tap } from 'rxjs';
 export class SelectorPageComponent implements OnInit {
 
   public countriesByRegion: SmallCoutry[] = [];
+  public borders: string[] = [];
 
   public myForm: FormGroup = this.fb.group({
     region: ['', Validators.required],
     country: ['', Validators.required],
-    borders: ['', Validators.required],
+    border: ['', Validators.required],
   });
 
   constructor (
@@ -28,22 +29,34 @@ export class SelectorPageComponent implements OnInit {
 
   ngOnInit(): void {
     this.onRegionChanged();
-
-
+    this.onCountryChanged();
   }
 
   get regions(): Region[] {
     return this.countriesService.regions;
   }
 
-  onRegionChanged() {
+  onRegionChanged(): void {
     this.myForm.get('region')!.valueChanges
     .pipe(
       tap(() => this.myForm.get('country')!.setValue('')),
+      tap(() => this.borders = []),
       switchMap((region) => this.countriesService.getCoutriesByRegion(region))
     )
     .subscribe(countries => {
       this.countriesByRegion = countries;
+    });
+  }
+
+  onCountryChanged(): void {
+    this.myForm.get('country')!.valueChanges
+    .pipe(
+      tap(() => this.myForm.get('border')!.setValue('')),
+      filter((value: string) => value.length > 0),
+      switchMap((alphaCode) => this.countriesService.getCountryByAlphaCode(alphaCode))
+    )
+    .subscribe(country => {
+      this.borders = country.borders;
     });
   }
 
